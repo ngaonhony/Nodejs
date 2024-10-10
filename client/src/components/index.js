535b70dffc45cfd9c171afd3df8954582936eef7
KhanhDepTrai <nguyengiakhanhqq@gmail.com>
2024-10-10 09:31:44 +0700

    fix rac

--- client/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Button} from './Button'
-export { default as Searchitem} from './Searchitem'
-export { default as ProvinceBtn} from './ProvinceBtn'
-export { default as Province} from './Province'
-export { default as Item} from './Item'
-export { default as Itemsidebar} from './Itemsidebar'
